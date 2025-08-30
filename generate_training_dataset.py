b05f75274c84c3dd88f5ad5179fcfbd8bc272a8c
Megha-Raj18 <rmegha5.2003@gmail.com>
2025-08-29 23:10:35 -0400

    generated training file based on pose_train.txt pkl files

--- generate_training_dataset.py
+++ generate_training_dataset.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 import torch
 
-split_file_path = "aist_plusplus_final\\splits\\pose_train_testing.txt"
+split_file_path = "aist_plusplus_final\\splits\\pose_train.txt"
 keypoints_path = "aist_plusplus_final\\keypoints3d"
 motions_path = "aist_plusplus_final\\motions"
 num_frames = 300
@@ -59,12 +59,31 @@ def main():
     seq_ids = load_split(split_file_path)
     print(f"Loaded {len(seq_ids)} training sequences")
 
+    data = []
+    labels = []
+    genres = []
+
     for i, seq_id in enumerate(seq_ids):
         pose_data, genre = process_sequence(seq_id, keypoints_path, motions_path, num_frames)
-        print(f"\nSequence {i+1}: {seq_id}")
-        print(f"  Genre: {genre}")
-        print(f"  Pose data shape: {pose_data.shape}")  # (frames, 17, 3)
-        print(f"  Example frame[0]:\n{pose_data[0]}")
+        data.append(pose_data)
+        labels.append(genre)
+        genres.append(genre)
 
+        print(f"Processed {i + 1}/{len(seq_ids)} : {seq_id}, genre={genre}, shape={pose_data.shape}")
+        unique_genres = sorted(set(genres))
+        genre_to_index = {genre: i for i , genre in enumerate(unique_genres)}
+        numeric_labels = [genre_to_index[genre] for genre in labels]
+
+        data_tensor = torch.tensor(np.stack(data), dtype = torch.float32)
+        labels_tensor = torch.tensor(numeric_labels, dtype = torch.long)
+
+        torch.save({
+            "data" : data_tensor,
+            "labels" : labels_tensor,
+            "genre_to_index" : genre_to_index
+        }, output_path)
+
+        print("Saved dataset")
+        print(f"Genres: {genre_to_index}")
 if __name__ == "__main__":
     main()
